ca54af9c5f4c77ed551a9d228df70a59f6c1cd59
minzhang <569835014@qq.com>
2019-09-18 02:52:46 +0800

    feat(首页开发):

--- src/config.js
+++ src/config.js
@@ -1,47 +1,48 @@
-const { resolve } = require('path')
-const r = url => resolve(__dirname, url)
-const assetsPath = resolve(process.cwd(), './build')
+const { resolve } = require('path');
+const r = url => resolve(__dirname, url);
+const assetsPath = resolve(process.cwd(), './build');
 
 module.exports = {
-    "json": {
-        "pages":[
-            "pages/home/home",
-        ],
-        "components":[
-            "components/add-button/add-button",
-            "components/navigation/navigation",
-        ],
-        "window": {
-
-        },
-        "tabBar": {
-            "textColor": "#999999",
-            "selectedColor": "#1B82D2",
-            "backgroundColor": "#FFFFFF",
-            "items": [
-                {
-                    "pagePath": "pages/index/index",
-                    "name": "首页",
-                    "icon": "images/tab-bar/index.png",
-                    "activeIcon": "images/tab-bar/index-active.png"
-                },
-                {
-                    "pagePath": "pages/title/title",
-                    "name": "抬头",
-                    "icon": "images/tab-bar/title.png",
-                    "activeIcon": "images/tab-bar/title-active.png"
-                },
-                {
-                    "pagePath": "pages/list/list",
-                    "name": "我的发票",
-                    "icon": "images/tab-bar/list.png",
-                    "activeIcon": "images/tab-bar/list-active.png"
-                }
-            ]
-        }
-    },
-    "entry":[
+  json: {
+    pages: [
+      'pages/home/home',
+      'pages/disc/disc',
     ],
-    "assetsPath": assetsPath,
-    "app": r('./app.js')
-}
+    components: [
+      'components/add-button/add-button',
+      'components/navigation/navigation',
+    ],
+    window: {
+
+    },
+    tabBar: {
+      textColor: '#999999',
+      selectedColor: '#1B82D2',
+      backgroundColor: '#FFFFFF',
+      items: [
+        {
+          pagePath: 'pages/index/index',
+          name: '首页',
+          icon: 'images/tab-bar/index.png',
+          activeIcon: 'images/tab-bar/index-active.png',
+        },
+        {
+          pagePath: 'pages/title/title',
+          name: '抬头',
+          icon: 'images/tab-bar/title.png',
+          activeIcon: 'images/tab-bar/title-active.png',
+        },
+        {
+          pagePath: 'pages/list/list',
+          name: '我的发票',
+          icon: 'images/tab-bar/list.png',
+          activeIcon: 'images/tab-bar/list-active.png',
+        },
+      ],
+    },
+  },
+  entry: [
+  ],
+  assetsPath: assetsPath,
+  app: r('./app.js'),
+};
