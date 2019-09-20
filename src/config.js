a378826938ab343b5100ef1eb35be8b0574aa301
zm569835014 <569835014@qq.com>
2019-09-20 22:06:37 +0800

    feat: 滚动测试

--- src/config.js
+++ src/config.js
@@ -3,46 +3,51 @@ const r = url => resolve(__dirname, url);
 const assetsPath = resolve(process.cwd(), './build');
 
 module.exports = {
-  json: {
-    pages: [
-      'pages/home/home',
-      'pages/disc/disc',
-    ],
-    components: [
-      'components/add-button/add-button',
-      'components/navigation/navigation',
-    ],
-    window: {
-
+    json: {
+        pages: [
+            'pages/home/home',
+            'pages/disc/disc',
+            'pages/h5/h5',
+            'pages/demo/demo',
+            'pages/scroll/scroll',
+        ],
+        components: [
+            'components/add-button/add-button',
+            'components/navigation/navigation',
+            'components/scroll/scroll',
+        ],
+        window: {
+            "disableScroll": true,
+            allowsBounceVertical: 'NO',
+            pullRefresh: false,
+        },
+        tabBar: {
+            textColor: '#999999',
+            selectedColor: '#1B82D2',
+            backgroundColor: '#FFFFFF',
+            items: [
+                {
+                    pagePath: 'pages/index/index',
+                    name: '首页',
+                    icon: 'images/tab-bar/index.png',
+                    activeIcon: 'images/tab-bar/index-active.png',
+                },
+                {
+                    pagePath: 'pages/title/title',
+                    name: '抬头',
+                    icon: 'images/tab-bar/title.png',
+                    activeIcon: 'images/tab-bar/title-active.png',
+                },
+                {
+                    pagePath: 'pages/list/list',
+                    name: '我的发票',
+                    icon: 'images/tab-bar/list.png',
+                    activeIcon: 'images/tab-bar/list-active.png',
+                },
+            ],
+        },
     },
-    tabBar: {
-      textColor: '#999999',
-      selectedColor: '#1B82D2',
-      backgroundColor: '#FFFFFF',
-      items: [
-        {
-          pagePath: 'pages/index/index',
-          name: '首页',
-          icon: 'images/tab-bar/index.png',
-          activeIcon: 'images/tab-bar/index-active.png',
-        },
-        {
-          pagePath: 'pages/title/title',
-          name: '抬头',
-          icon: 'images/tab-bar/title.png',
-          activeIcon: 'images/tab-bar/title-active.png',
-        },
-        {
-          pagePath: 'pages/list/list',
-          name: '我的发票',
-          icon: 'images/tab-bar/list.png',
-          activeIcon: 'images/tab-bar/list-active.png',
-        },
-      ],
-    },
-  },
-  entry: [
-  ],
-  assetsPath: assetsPath,
-  app: r('./app.js'),
+    entry: [],
+    assetsPath: assetsPath,
+    app: r('./app.js'),
 };
